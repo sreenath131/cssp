225ec6fe702b97afce43aadb263a5e88778fecb4
Sayali Dhumal <sayalidhumal@Sayalis-MacBook-Pro.local>
2017-05-15 18:37:10 -0700

    some changes

--- www/app/home/home.controller.js
+++ www/app/home/home.controller.js
@@ -7,20 +7,21 @@
         var vm = this;
         vm.apps = [
             {
-                Name: 'Application 1',
-                State: ''
+                Name: "Toastmasters at CSUSB",
+                President_First_Name: "Christopher",
+                President_Last_Name: "Evelynveere",
+                President_Cell_Phone_Number: "951-534-3066",
+                President_CSUSB_Email_Address: "cgeveere@hotmail.com",
+                Category: "Social Clubs & Organizations"
             },
+
             {
-                Name: 'Application 2',
-                State: ''
-            },
-            {
-                Name: 'Application 3',
-                State: ''
-            },
-            {
-                Name: 'Application 4',
-                State: ''
+                Name: "Inland Empire Future Leaders",
+                President_First_Name: "Raylene",
+                President_Last_Name: "Perez",
+                President_Cell_Phone_Number: "909-380-1814",
+                President_CSUSB_Email_Address: "perer355@coyote.csusb.edu",
+                Category: "Service or Support Organizations"
             }
         ]
     }
